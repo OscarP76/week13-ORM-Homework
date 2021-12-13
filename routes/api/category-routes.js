0f2b172abc9b47a040d00ed1f3f9e045ba6089bf
Oscar Plumridge <Bigop76.op@gmail.com>
2021-12-13 10:59:40 -0800

    made all the routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,54 +6,67 @@ const { Category, Product } = require('../../models');
 router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
-  
-    try {
-      const category = await Category.findAll({
-        include: [{ model: Product }],
-      });
-      res.status(200).json(category);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-    
+  try {
+    const category = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  router.get('/:id', async (req, res) => {
-    try {
-      const singleCategory = await Category.findByPk(req.params.id, {
-        include: [{ model: Product }],
-      });
-  
-      if (!singleCategory) {
-        res.status(404).json({ message: 'finding one category by id' });
-        return;
-      }
-  
-      res.status(200).json(singleCategory);
-    } catch (err) {
-      res.status(500).json(err);
+// find one category by its `id` value
+// be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const singleCategory = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!singleCategory) {
+      res.status(404).json({ message: 'finding one category by id' });
+      return;
     }
-  });
 
-  // create a new category
-  router.post('/', async (req, res) => {
-    try {
-      // Since the model will create a unique UUID value by default, we just need to provide the `id` of the Reader that will own this card
-      const newCategory = await Category.create({
-        category_id: req.body.category_id,
-      });
-      res.status(200).json(newCategory);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+    res.status(200).json(singleCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create a new category
+router.post('/', async (req, res) => {
+  try {
+    // Since the model will create a unique UUID value by default, we just need to provide the `id` of the Reader that will own this card
+    const newCategory = await Category.create({
+      category_name: req.body.category_name,
+    });
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const updateCategory = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!updateCategory) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+    res.status(200).json(updateCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', async (req, res) => {
@@ -65,7 +78,7 @@ router.delete('/:id', async (req, res) => {
       },
     });
 
-    if(deleteCategory) {
+    if (!deleteCategory) {
       res.status(404).json({ message: 'No category found with that id!' });
       return;
     }
